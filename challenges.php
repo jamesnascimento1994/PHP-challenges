37f17596416a7adceced1c71d4b1cdd1586a90b2
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-03 14:19:03 -0500

    solution for buggy code

---
+++ challenges.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+/* 
+Challenge One:  This code does not execute properly. Try to figure out why.
+Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/solutions/php      
+*/
+
+// Starter Code
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// Solution
+// function multiply($a, $b) {
+//     return $a * $b;
+// }
+
+// TESTS
+// echo multiply(4,5);
+// echo multiply(6,3); 
+// echo multiply(2,1);
+?>
+</body>
+</html>
