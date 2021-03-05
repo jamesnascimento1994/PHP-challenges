173204a81c4684c7371710c9eb8be8a37bc296e6
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-04 22:39:16 -0500

    solution for hello function

--- challenges.php
+++ challenges.php
@@ -23,10 +23,28 @@ Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/solutio
 //     return $a * $b;
 // }
 
-// TESTS
+// // TESTS
 // echo multiply(4,5);
 // echo multiply(6,3); 
 // echo multiply(2,1);
+
+/* 
+Challenge Two: Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String). Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+Challenge Source: https://www.codewars.com/kata/57e3f79c9cb119374600046b/php
+*/
+
+// function hello($name = ''): string {
+//     if ($name) {
+//         return 'Hello, '.strtoupper(substr($name, 0, 1)).''.strtolower(substr($name, 1).'!');
+//     } else {
+//         return 'Hello, World!';
+//     }
+// }
+
+// // TESTS
+// echo hello('jImMy');
+// echo hello('');
+// echo hello();
 ?>
 </body>
 </html>
