2c45fe055b3a8d16a599fab02f29e6b47385eef3
jamesnascimento1994 <james.nascimento94@yahoo.com>
2022-01-07 20:50:20 -0500

    added jaden case solution

--- challenges.php
+++ challenges.php
@@ -86,22 +86,37 @@ Challenge Five: Your task is to make a function that can take any non-negative i
 Challenge Source: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 */
 
-function descendingOrder(int $n): int {
-    // Replace with your code
-    $parseFloat = floatval($n);
-    $toString = strval($parseFloat);
-    $length = 1;
-    $splitStr = str_split($toString, $length);
-    rsort($splitStr);
-    $joined = implode($splitStr);
-    $sign = $n <=> 0;
-    return $joined * $sign;
-  }
+// function descendingOrder(int $n): int {
+//     // Replace with your code
+//     $parseFloat = floatval($n);
+//     $toString = strval($parseFloat);
+//     $length = 1;
+//     $splitStr = str_split($toString, $length);
+//     rsort($splitStr);
+//     $joined = implode($splitStr);
+//     $sign = $n <=> 0;
+//     return $joined * $sign;
+//   }
 
-  // TESTS
-  echo descendingOrder(42145);
-  echo descendingOrder(145263);
-  echo descendingOrder(123456789);
+//   // TESTS
+//   echo descendingOrder(42145);
+//   echo descendingOrder(145263);
+//   echo descendingOrder(123456789);
+
+/* 
+Challenge Six: Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below. Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Challenge Source: https://www.codewars.com/kata/5390bac347d09b7da40006f6
+*/
+
+function toJadenCase($string) 
+{
+   //...
+  return ucwords($string);
+}
+
+// TESTS
+toJadenCase("How can mirrors be real if our eyes aren't real");
 ?>
 </body>
 </html>
